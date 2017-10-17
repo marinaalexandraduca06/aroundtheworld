eaf442477f69aacfef297bc30fb0995175fb2d39
marinaalexandraduca06 <marinaalexandraduca06@gmail.com>
2017-10-17 13:28:30 +0300

    modified .window

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sign-up',
-  templateUrl: 'signup.component.html'
+  templateUrl: 'signup.component.html',
+  styleUrls: ['signup.component.css']
 })
 
 export class SignupComponent {}
